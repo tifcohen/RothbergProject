83d5b3fbcf16a50e724570bf2e74ffbe711dacaa
tifcohen <tifcohen@gmail.com>
2025-11-26 10:43:28 +0200

    initial commit - basic app, 2 activities, lifecycle example

---
+++ app/src/main/java/com/example/layoutexample/ui/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.layoutexample.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.layoutexample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG  = "MyMainActivity";
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate");
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Button Clicked!");
+                openSecondActivity();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy");
+    }
+
+    private void openSecondActivity() {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        Log.d(TAG, "Open second activity...");
+        startActivity(intent);
+    }
+}
